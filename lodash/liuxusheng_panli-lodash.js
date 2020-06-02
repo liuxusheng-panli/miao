f8cacaa02dc738bdfd02650c54fb647e15ab2a31
liuxusheng <1213192904@qq.com>
2020-06-02 15:30:04 +0800

    lodash

--- lodash/liuxusheng_panli-lodash.js
+++ lodash/liuxusheng_panli-lodash.js
@@ -1,25 +1,11 @@
 var liuxusheng = {
-  isNull: function (val) {
-    if (val === null) {
-      return true
-    } else {
-      return false
-    }
-  },
   isNaN: function (val) {
-    if (val !== val) {
-      return true
+    if (val != val && Number.isNaN(val)) {
+      return true;
+    } else if (typeof val == 'object') {
+      return val.toString() === 'NaN';
     } else {
-      return false
+      return false;
     }
-  },
-  chunk: function (ary, size) {
-    let result = [];
-
-    for (let i = 0; i < ary.length; i += size) {
-      result.push(ary.slice(i, i + size))
-    }
-
-    return result;
   },
 }
