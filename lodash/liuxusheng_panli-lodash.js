65b9d39eec7aada549028cf3927424e76733b0e7
liuxusheng <1213192904@qq.com>
2020-06-02 20:09:43 +0800

    lodash

--- lodash/liuxusheng_panli-lodash.js
+++ lodash/liuxusheng_panli-lodash.js
@@ -30,6 +30,43 @@ var liuxusheng_panli = {
     }
     return temp
   },
+  // difference: function(arr,values){
+  //   var temp = arr
+  //   var temp2 = values
+  //   var temp3 = arr.concat(values)
+  //   var map = {}
+  //   for (i = 0; i < temp3.length; i++){
+  //     var con = temp[i]
+  //     if (con in map){
+  //       map[con]++
+  //     } else{
+  //       map[con] = 1
+  //     }
+  //   }
+  //   for (i = 0; i < temp3.length; i++){
+  //     var con = temp[i]
+  //     if (map[c] == )
+  //   }
+
+  difference: (array, ...args) {
+    let map = {};
+    let ans = [];
+    for (let i = 0; i < array.length; i++) {
+      map[array[i]] = true;
+    }
+    for (let i in args) {
+      for (let j in args[i]) {
+        if (map[args[i][j]]) map[args[i][j]] = false;
+      }
+    }
+    for (let i in array) {
+      if (map[array[i]]) {
+        ans[ans.length] = array[i];
+      }
+    }
+    return ans;
+  }
+
 }
 
 
