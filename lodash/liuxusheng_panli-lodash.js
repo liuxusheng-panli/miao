aef4051d293fc38a957538299f2801fc9657e416
liuxusheng <1213192904@qq.com>
2020-06-04 22:27:34 +0800

    lod

--- lodash/liuxusheng_panli-lodash.js
+++ lodash/liuxusheng_panli-lodash.js
@@ -87,13 +87,20 @@ var liuxusheng_panli = {
   },
 
   indexOf: function (array, num, target = 0) {
-    for (var i = target; i < array.length; i++) {
-      if (array[i] === num) {
-        return i
-      } else {
-        return -1
+    if (isNaN(value)) {
+      for (var i = target; i < array.length; i++) {
+        if (isNaN(array[i])) {
+          return i
+        }
+      }
+    } else {
+      for (var i = target; i < array.length; i++) {
+        if (array[i] === num) {
+          return i
+        }
       }
     }
+    return -1
   },
 }
 
